6daa9d2311614ea42a78dee041882f748771284e
mkotsk <127721977+mkotsk@users.noreply.github.com>
2023-03-14 04:24:01 +0000

    first init

--- utils/global-data.js
+++ utils/global-data.js
@@ -7,7 +7,7 @@ export const getGlobalData = () => {
     : 'Next.js Blog Theme';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : 'All rights reserved...';
 
   return {
     name,
